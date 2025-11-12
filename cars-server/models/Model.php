b32f21bde38d66c4236f73a6c25b1a2b7ad5e067
joyagergess <gergessjoya@gmail.com>
2025-11-12 16:54:15 +0200

    Started the create funtion in Model

--- cars-server/models/Model.php
+++ cars-server/models/Model.php
@@ -2,14 +2,13 @@
 abstract class Model{
 
     protected static string $table;
-    //protected static string $primary_key = "id";
+   
 
     public static function find(mysqli $connection, string $id, string $primary_key = "id"){
         $sql = sprintf("SELECT * from %s WHERE %s = ?",
                        static::$table,
                        $primary_key);
-                       //static::$primary_key);
-
+                     
         $query = $connection->prepare($sql);
         $query->bind_param("i", $id);
         $query->execute();               
@@ -34,9 +33,20 @@ abstract class Model{
         return $rows;
     }
 
+    public function create($table, $data,mysqli $connection) {
+
+        $columns = implode(', ', array_keys($data));
+
+        $placeholders = implode (',' ,array_fill(0, count($data),?));
+
+        $sql = "INSERT INTO $table ($columns) VALUES ($placeholders)";
+        $result = $connection->prepare($sql);
+        
+         $result ->bind_param()
+        return $result->execute();
 
 }
 
 
-
+}
 ?>
